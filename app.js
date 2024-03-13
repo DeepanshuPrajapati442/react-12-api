48228e0f717a7bd5329cd13d1f2a10a26f509e4e
deepanshu prajapati <deepanshuprajapati442@gmail.com>
2024-03-13 21:12:32 +0530

    uploaded

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT;
+//db connection
+require("./models/dbconfig").dbconnection();
 
-app .listen(PORT , ()=>{
-console.log(`the server is running on port${PORT}`);
+app.listen(PORT , ()=>{
+console.log(`The server is running on port${PORT}`);
 
 });
